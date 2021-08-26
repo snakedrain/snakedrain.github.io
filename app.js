ea7ed4bbb1f36d2aa438d4dda8d94a174e90da69
thisispriyanshu <priyanshuagrawal801@gmail.com>
2021-08-26 21:53:23 +0530

    Updated quit key

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
-// pen.fillRect(rect.x,rect.y,rect.w,rect.h)      
-function f(){
-    console.log("exit")
+function q(e){
+    if(e.key=='q')
+    game_over=true
+    console.log(e.key)
 }
 
 function init(){
-    // console.log("IN")
     canvas=document.getElementById("mycanvas")
 pen=canvas.getContext('2d')
 rect = {
@@ -19,32 +18,25 @@ game_over=false
 }
 
 function draw(){
-    // console.log("This is called ")
-// console.log("DRAW")
 pen.fillStyle="red"
 pen.fillRect(rect.x,rect.y,rect.w,rect.h)      
 pen.clearRect(rect.x-rect.speed,rect.y,rect.w,rect.h)      
-
 }
     function update(){
-        // console.log("Update")
-        // while(rect.x<500){
-        
        rect.x=rect.x+rect.speed
        if(rect.x>canvas.width-rect.w)
        rect.speed *= -1
        if(rect.x<0)
        rect.speed *=-1
-
-        // }
     }
-
 function gameloop(){
     draw()
     update()
     if(game_over==true)
-clearInterval(f)
+    clearInterval(f)
 }
 init()
 var f=setInterval(gameloop,100)
+document.addEventListener('keydown',q)
+
 
