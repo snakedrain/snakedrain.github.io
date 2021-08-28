5fff5c6256f4bade4bdb97e8afd8c18a696e0a55
thisispriyanshu <priyanshuagrawal801@gmail.com>
2021-08-29 01:21:15 +0530

    Update updateSnake

--- app.js
+++ app.js
@@ -7,15 +7,8 @@ function q(e) {
 function init() {
     canvas = document.getElementById("mycanvas")
     pen = canvas.getContext('2d')
-    cs_w=20
-    cs_h=10
-    rect = {
-        w: 20,
-        h: 10,
-        x: 100,
-        y: 100,
-        speed: 20,
-    }
+    cs=15
+    
     game_over = false
 
     snake={
@@ -31,32 +24,33 @@ function init() {
 
         drawSnake:function(){
            for(var i=0;i<this.init_len;i++)
-           pen.fillRect(this.cells[i].x*cs_w,this.cells[i].y*cs_h,cs_w-1,cs_h-1)
+           pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-1,cs-1)
         },
 
         updateSnake:function(){
-            for(var i=0;i<this.init_len;i++)
-            (this.cells[i].x) += cs_w
+            this.cells.pop()
+            // console.log("poppping")
+            var headX= this.cells[0].x;
+            var headY= this.cells[0].y;
+            var X = headX+1;
+            var Y= headY;
+            this.cells.unshift({x:X,y:Y})
         }
     }
 snake.createSnake()
+
 }
 
 function draw() {
+    // console.log("drawing")
+    pen.clearRect(0,0,500,500)
     snake.drawSnake()
-    pen.fillStyle = "red"
-    pen.fillRect(rect.x, rect.y, rect.w, rect.h)
-    pen.clearRect(rect.x - rect.speed, rect.y, rect.w, rect.h)
 }
 
 function update() {
- 
+    // console.log("UPDATING...") 
     snake.updateSnake()
-    rect.x = rect.x + rect.speed
-    if (rect.x > canvas.width - rect.w)
-        rect.speed *= -1
-    if (rect.x < 0)
-        rect.speed *= -1
+   
 }
 
 function gameloop() {
@@ -67,6 +61,7 @@ function gameloop() {
 }
 
 init()
+
 var f = setInterval(gameloop, 100)
 document.addEventListener('keydown', q)
 
